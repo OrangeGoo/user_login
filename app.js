de68de5fcdcfdb43dd26d726175c8f1105be5c18
kynewbee <justin.chen0420@gmail.com>
2024-10-21 19:35:21 -0400

    all methods middleware

--- app.js
+++ app.js
@@ -3,6 +3,18 @@ const path = require('path');
 const app = express();
 
 
+app.use((req, res, next) => {
+  const startTime = Date.now();
+  console.log(`Incoming Request: Method=${req.method}, Path=${req.path}`);
+
+  res.on('finish', () => {
+    const duration = Date.now() - startTime;
+    console.log(`Request Processed: Method=${req.method}, Path=${req.path}, Status=${res.statusCode}, Duration=${duration}ms`);
+  });
+
+  next();
+});
+
 const publicDirectory = path.join(__dirname, './public');
 app.use(express.static(publicDirectory));
 
